17d55b34a5531871cf40db278ac0a13773f89c9c
Luz Hernandez <hernandezluz18@gmail.com>
2024-05-20 21:21:37 -0400

    added fetch method to access API endpoints from react code

--- music_room/frontend/src/components/App.js
+++ music_room/frontend/src/components/App.js
@@ -1,24 +1,11 @@
 import React from "react";
 import Homepage from "./Homepage";
-import RoomJoinPage from "./RoomJoinPage";
-import CreateRoomPage from "./CreateRoomPage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Redirect,
-} from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" Component={Homepage}></Route>
-        <Route path="/join" Component={RoomJoinPage}></Route>
-        <Route path="/create" Component={CreateRoomPage}></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Homepage />
+    </div>
   );
 };
 
--- music_room/frontend/src/components/Homepage.js
+++ music_room/frontend/src/components/Homepage.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Room from "./Room";
+import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div>
-      <p>This is the HomePage component</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/"></Route>
+        <Route path="/join" element={<RoomJoinPage />} />
+        <Route path="/create" element={<CreateRoomPage />} />
+        <Route path="/room/:roomCode" element={<Room />} />
+      </Routes>
+    </Router>
   );
 };
 
