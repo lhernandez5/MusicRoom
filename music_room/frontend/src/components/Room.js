5f12b4dcde8b50a61a166b8b060ee7bff714dce6
Luz Hernandez <hernandezluz18@gmail.com>
2024-05-28 10:20:49 -0400

    updated the room settings model and view

--- music_room/frontend/src/components/Room.js
+++ music_room/frontend/src/components/Room.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoomPage from "./CreateRoomPage";
 
 const Room = ({ leaveRoomCallback }) => {
   const [votesToSkip, setVotesToSkip] = useState(2);
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [isHost, setIsHost] = useState(false);
   const { roomCode } = useParams();
+  const [showSettings, setShowSettings] = useState(false);
   const leaveRoomCallbackProp = leaveRoomCallback;
   const navigate = useNavigate();
 
@@ -43,41 +45,89 @@ const Room = ({ leaveRoomCallback }) => {
     });
   };
 
-  return (
-    <div>
+  const updateShowSettings = (value) => {
+    setShowSettings(value);
+  };
+
+  const renderSettings = () => {
+    return (
       <Grid container spacing={1}>
         <Grid item xs={12} align="center">
-          <Typography variant="h4" component="h4">
-            Code: {roomCode}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Votes to Skip: {votesToSkip}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Guest can pause: {guestCanPause}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Host: {isHost}
-          </Typography>
+          <CreateRoomPage
+            update={true}
+            votesToSkip={votesToSkip}
+            guestCanPause={guestCanPause}
+            roomCode={roomCode}
+            updateCallback={""}
+          />
         </Grid>
         <Grid item xs={12} align="center">
           <Button
             variant="contained"
             color="secondary"
-            onClick={leaveButtonPressed}
+            onClick={() => updateShowSettings(false)}
           >
-            Leave Room
+            Close
           </Button>
         </Grid>
       </Grid>
-    </div>
-  );
+    );
+  };
+
+  const renderSettingsButton = () => {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  };
+
+  if (showSettings) {
+    return renderSettings();
+  } else {
+    return (
+      <div>
+        <Grid container spacing={1}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h4" component="h4">
+              Code: {roomCode}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Votes to Skip: {votesToSkip}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Guest can pause: {guestCanPause}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Typography variant="h6" component="h6">
+              Host: {isHost}
+            </Typography>
+          </Grid>
+          {isHost ? renderSettingsButton() : null}
+          <Grid item xs={12} align="center">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={leaveButtonPressed}
+            >
+              Leave Room
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 };
 
 export default Room;
